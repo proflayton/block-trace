fddc2dbfffa18daa563b5c997a2ccf39475b97c3
Brandon Layton <blayton@uber.com>
2016-08-30 17:20:53 -0700

    Make Compatible

--- bin.js
+++ bin.js
@@ -7,6 +7,7 @@ var fs = require('fs')
 var path = require('path')
 var os = require('os')
 
+var tempName = null;
 var child = null
 var debug = null
 var tracing = false
@@ -27,7 +28,7 @@ function whichAndRun (args) {
 }
 
 function run (args) {
-  args.unshift('--debug', '-r', require.resolve('./trace.js'))
+  args.unshift('--debug')
 
   var server = unref(net.createServer(onsocket))
   var timeout = unref(setTimeout(ontimeout, 1000))
@@ -38,7 +39,26 @@ function run (args) {
     process.env.BLOCK_TRACE_PORT = '' + server.address().port
     process.env.BLOCK_TRACE_FILE = path.join(os.tmpDir(), 'block-trace-' + server.address().port)
 
-    child = proc.spawn(process.execPath, args, {stdio: ['inherit', 'inherit', null]})
+    var moduleToRun = args[1];
+    tempName = "tmp_" + moduleToRun;
+
+    var exists = fs.existsSync(path.join(__dirname, tempName));
+    if (exists) {
+      fs.unlinkSync(path.join(__dirname, tempName));
+    }
+    var fileData = fs.readFileSync(path.join(__dirname, moduleToRun));
+    fs.writeFileSync(
+      path.join(__dirname, tempName),
+      "require('" + require.resolve('./trace.js') + "');\n" + fileData,
+      {
+        flags: 'w'
+      }
+    );
+    args[1] = tempName;
+
+    child = proc.spawn(process.execPath, args, {
+      stdio: [process.stdin, process.stdout, null]
+    })
     child.on('exit', cleanup)
 
     child.stderr.once('data', function (line) {
@@ -63,17 +83,31 @@ function destroy () {
 
 function ontimeout () {
   if (!child) return
-
   tracing = true
-  debug = proc.spawn(process.execPath, ['debug', '-p', child.pid])
-  debug.stdin.write('repl\n')
-  debug.stdin.write('WRITE_BLOCK_TRACE_FILE()\n')
+  debug = proc.spawn(process.execPath, ['debug', 'localhost:5858'])
+  debug.stdout.once('data', _checkData)
+  function _checkData(data) {
+    if (data.toString().indexOf("ok") !== -1) {
+      setImmediate(_write)
+    } else {
+      debug.stdout.once('data', _checkData)
+    }
+  }
+  function _write() {
+    debug.stdin.write('repl\nWRITE_BLOCK_TRACE_FILE()\n');
+  }
   debug.on('exit', cleanup)
 }
 
 function cleanup () {
   if (child) child.kill()
   if (debug) debug.kill()
+  if (tempName) {
+    var exists = fs.existsSync(path.join(__dirname, tempName));
+    if (exists) {
+      fs.unlinkSync(path.join(__dirname, tempName));
+    }
+  }
 
   if (!tracing) return
   tracing = false
--- trace.js
+++ trace.js
@@ -1,20 +1,24 @@
+var stackTrace = require('stack-trace')
 var net = require('net')
 var fs = require('fs')
-
-var socket = unref(net.connect(process.env.BLOCK_TRACE_PORT))
-unref(setInterval(ping, 500))
+var socket = net.connect({
+	port: process.env.BLOCK_TRACE_PORT
+})
+var interval = setInterval(ping, 500)
 ping()
-
-function unref (obj) {
-  if (obj.unref) obj.unref()
-  return obj
-}
+interval.unref()
+socket.unref()
 
 function ping () {
   socket.write('ping\n')
 }
 
 global.WRITE_BLOCK_TRACE_FILE = function () {
-  fs.writeFileSync(process.env.BLOCK_TRACE_FILE, new Error('noop').stack.toString())
-  process.exit(1)
+  var stack = stackTrace.parse(new Error());
+  var string = "";
+  for (var s in stack) {
+  	string += JSON.stringify(stack[s]) + "\n"
+  }
+  fs.writeFileSync(process.env.BLOCK_TRACE_FILE, string, 'utf8');
+  process.exit(1);
 }
